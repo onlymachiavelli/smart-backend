9d7ef3b07138cd998695162986237fb8e9d6d372
onlymachiavelli <tmakaveli643@gmail.com>
2022-09-22 15:24:06 +0100

    connecting to the db

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express from "express"
 import "dotenv/config"
 
 import { json } from "body-parser"
+
+import appDataSource from "./utils/POSTGRES"
+
 const cors = require("cors")
 
 const app = express()
@@ -14,6 +17,16 @@ const PORT: any = process.env.PORT || 3000
 app
   .listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
+
+    appDataSource
+      .initialize()
+      .then((res) => {
+        //write the rest of the code here !
+        console.log("Connected to the database ! ")
+      })
+      .catch((e) => {
+        console.log(e)
+      })
   })
   .on("error", () => {
     console.log("There's an error ! ")
