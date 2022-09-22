6344b97e045197ad29500becc3a5fefd1d11c11f
onlymachiavelli <tmakaveli643@gmail.com>
2022-09-22 14:13:08 +0100

    admin model

--- src/models/admins.ts
+++ src/models/admins.ts
@@ -1,6 +1,53 @@
 import { Entity, BaseEntity, Column, PrimaryColumn } from "typeorm"
 
 @Entity()
-class Admins extends BaseEntity {}
+class Admins extends BaseEntity {
+  @PrimaryColumn()
+  cin: String
+
+  @Column({
+    nullable: false,
+    unique: false,
+  })
+  fullname: String
+
+  @Column({
+    nullable: false,
+    unique: true,
+  })
+  email: String
+
+  @Column({
+    nullable: false,
+    unique: true,
+  })
+  phone: String
+
+  @Column({
+    nullable: false,
+    unique: false,
+    enum: ["male", "female"],
+  })
+  gender: String
+
+  @Column({
+    nullable: false,
+    unique: false,
+  })
+  adress: String
+
+  @Column({
+    nullable: false,
+    unique: false,
+  })
+  createdat: Date
+
+  @Column({
+    nullable: false,
+    unique: false,
+    enum: ["boss", "seller", "prodStock"],
+  })
+  role: String
+}
 
 export default Admins
